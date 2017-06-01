cf99d881db0f9ad5ea8341472c8eb4e97738f339
Vimalesan <vimalesan.ceg@gmail.com>
2017-06-01 11:29:00 +0530

    docs - svg issue cors fix

--- src/config/webpack.dev.config.js
+++ src/config/webpack.dev.config.js
@@ -13,85 +13,86 @@ var appFolder = process.env.npm_config_app_folder || 'src';
 var fs = require('fs');
 var appPath = fs.realpathSync(process.cwd());
 var isVendor = function isVendor(_ref) {
-	var userRequest = _ref.userRequest;
-	return userRequest && userRequest.indexOf('node_modules') >= 0;
+  var userRequest = _ref.userRequest;
+  return userRequest && userRequest.indexOf('node_modules') >= 0;
 };
 var isReact = function isReact(_ref) {
-	var userRequest = _ref.userRequest;
-	return userRequest && userRequest.indexOf('node_modules/react') >= 0;
+  var userRequest = _ref.userRequest;
+  return userRequest && userRequest.indexOf('node_modules/react') >= 0;
 };
 module.exports = {
-	entry: {
-		main: ['babel-polyfill', path.join(appPath, appFolder, 'index.js')]
-	},
-	devtool: 'eval',
-	output: {
-		path: path.resolve(appPath, 'build'),
-		filename: '[name].js',
-		publicPath: [url, context, 'js'].join('/')
-	},
-	plugins: [
-		new CaseSensitivePathsPlugin(),
-		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'vendor',
-			chunks: ['main'],
-			minChunks: isVendor
-		}),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'react.vendor',
-			chunks: ['vendor'],
-			minChunks: isReact
-		}),
-		new webpack.DefinePlugin({
-			__CLIENT__: true,
-			__TEST__: false,
-			__SERVER__: true,
-			__DEVELOPMENT__: true,
-			__DEVTOOLS__: true
-		})
-	],
-	module: {
-		rules: [
-			{
-				include: /\.json$/,
-				use: ['json-loader']
-			},
-			{
-				test: /\.jsx|\.js$/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								[require.resolve('babel-preset-es2015'), { modules: false }],
-								require.resolve('babel-preset-react')
-							],
-							cacheDirectory: true
-						}
-					}
-				],
-				include: path.join(appPath, appFolder)
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader?modules&localIdentName=[name]__[local]__[hash:base64:5]'
-				]
-			},
-			{
-				test: /\.jpe?g$|\.gif$|\.png$/,
-				use: ['url-loader?limit=1000&name=./images/[name].[ext]']
-			},
-			{
-				test: /\.woff$|\.ttf$|\.eot$/,
-				use: ['url-loader?limit=1000&name=./fonts/[name].[ext]']
-			},
-			{
-				test: /\.svg$/,
-				use: ['url-loader?limit=1&name=./fonts/[name].[ext]']
-			}
-		]
-	}
+  entry: {
+    main: ['babel-polyfill', path.join(appPath, appFolder, 'index.js')]
+  },
+  devtool: 'eval',
+  output: {
+    path: path.resolve(appPath, 'build'),
+    filename: '[name].js',
+    publicPath: [url, context, 'js'].join('/')
+  },
+  plugins: [
+    new CaseSensitivePathsPlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: ['main'],
+      minChunks: isVendor
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'react.vendor',
+      chunks: ['vendor'],
+      minChunks: isReact
+    }),
+    new webpack.DefinePlugin({
+      __CLIENT__: true,
+      __TEST__: false,
+      __SERVER__: true,
+      __DEVELOPMENT__: true,
+      __DEVTOOLS__: true,
+      __DOCS__: false
+    })
+  ],
+  module: {
+    rules: [
+      {
+        include: /\.json$/,
+        use: ['json-loader']
+      },
+      {
+        test: /\.jsx|\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [require.resolve('babel-preset-es2015'), { modules: false }],
+                require.resolve('babel-preset-react')
+              ],
+              cacheDirectory: true
+            }
+          }
+        ],
+        include: path.join(appPath, appFolder)
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules&localIdentName=[name]__[local]__[hash:base64:5]'
+        ]
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$/,
+        use: ['url-loader?limit=1000&name=./images/[name].[ext]']
+      },
+      {
+        test: /\.woff$|\.ttf$|\.eot$/,
+        use: ['url-loader?limit=1000&name=./fonts/[name].[ext]']
+      },
+      {
+        test: /\.svg$/,
+        use: ['url-loader?limit=1&name=./fonts/[name].[ext]']
+      }
+    ]
+  }
 };
--- src/config/webpack.docs.config.js
+++ src/config/webpack.docs.config.js
@@ -13,88 +13,89 @@ var appPath = fs.realpathSync(process.cwd());
 var moduleStatsPlugin = require('../moduleStatsPlugin');
 
 module.exports = {
-	entry: {
-		main: [
-			'babel-polyfill',
-			path.join(appPath, componentPath || 'src', 'index.js')
-		],
-		vendor: ['react', 'react-dom', 'redux', 'react-redux']
-	},
-	devtool: 'eval',
-	output: {
-		path: path.resolve(appPath, 'build'),
-		filename: '[name].js',
-		publicPath: url + '/docs/js',
-		library: 'Component',
-		libraryTarget: 'umd'
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			React: 'react'
-		}),
-		new CaseSensitivePathsPlugin(),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'vendor',
-			filename: 'vendor.js'
-		}),
-		new webpack.DefinePlugin({
-			__TEST__: false,
-			__DEVELOPMENT__: true
-		}),
-		new moduleStatsPlugin({ filename: 'moduleStats.js' })
-	],
-	module: {
-		rules: [
-			{
-				include: /\.json$/,
-				use: ['json-loader']
-			},
-			{
-				test: /\.jsx|\.js$/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								[require.resolve('babel-preset-es2015'), { modules: false }],
-								require.resolve('babel-preset-react')
-							],
-							cacheDirectory: true
-						}
-					}
-				],
-				include: path.join(appPath, 'src')
-			},
-			{
-				test: /\.docs\.jsx$|\.docs\.js$/,
-				use: require.resolve('../docsLoader.js'),
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader?modules&localIdentName=[name]__[local]__[hash:base64:5]'
-				]
-			},
-			{
-				test: /\.jpe?g$|\.gif$|\.png$/,
-				use: ['url-loader?limit=10000&name=./images/[name].[ext]']
-			},
-			{
-				test: /\.woff$|\.ttf$|\.eot$/,
-				use: ['url-loader?limit=10000&name=./fonts/[name].[ext]']
-			},
-			{
-				test: /\.svg$/,
-				use: ['url-loader?limit=1&name=./fonts/[name].[ext]']
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			react$: path.resolve(__dirname, '..', 'hook', 'proptypes.js')
-		}
-	}
+  entry: {
+    main: [
+      'babel-polyfill',
+      path.join(appPath, componentPath || 'src', 'index.js')
+    ],
+    vendor: ['react', 'react-dom', 'redux', 'react-redux']
+  },
+  devtool: 'eval',
+  output: {
+    path: path.resolve(appPath, 'build'),
+    filename: '[name].js',
+    publicPath: url + '/docs/js',
+    library: 'Component',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react'
+    }),
+    new CaseSensitivePathsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.js'
+    }),
+    new webpack.DefinePlugin({
+      __TEST__: false,
+      __DEVELOPMENT__: true,
+      __DOCS__: true
+    }),
+    new moduleStatsPlugin({ filename: 'moduleStats.js' })
+  ],
+  module: {
+    rules: [
+      {
+        include: /\.json$/,
+        use: ['json-loader']
+      },
+      {
+        test: /\.jsx|\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [require.resolve('babel-preset-es2015'), { modules: false }],
+                require.resolve('babel-preset-react')
+              ],
+              cacheDirectory: true
+            }
+          }
+        ],
+        include: path.join(appPath, 'src')
+      },
+      {
+        test: /\.docs\.jsx$|\.docs\.js$/,
+        use: require.resolve('../docsLoader.js'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules&localIdentName=[name]__[local]__[hash:base64:5]'
+        ]
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$/,
+        use: ['url-loader?limit=10000&name=./images/[name].[ext]']
+      },
+      {
+        test: /\.woff$|\.ttf$|\.eot$/,
+        use: ['url-loader?limit=10000&name=./fonts/[name].[ext]']
+      },
+      {
+        test: /\.svg$/,
+        use: ['url-loader?limit=1&name=./fonts/[name].[ext]']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      react$: path.resolve(__dirname, '..', 'hook', 'proptypes.js')
+    }
+  }
 };
