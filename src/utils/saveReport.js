b5357f79c912fcf0e25825c1d2de4e07f09bee2a
Vasikaran <vasikaran.s@zohocorp.com>
2017-08-01 12:56:37 +0530

    update saveReport.js

--- src/utils/saveReport.js
+++ src/utils/saveReport.js
@@ -1,32 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const {copy} = require('fz-fs-utils');
 
 var appPath = fs.realpathSync(process.cwd());
+let reportPath = path.resolve(appPath, 'performance-reports');
 
-function saveReport(report, name){
-
-    let reportPath = path.resolve(appPath, 'performance-reports');
-    if (!fs.existsSync(reportPath)){
-        fs.mkdirSync(reportPath);
-        let html = fs.readFileSync(path.resolve(__dirname, '../../performance-reports/report.html')).toString();
-        fs.writeFileSync(path.resolve(reportPath, './report.html'), html);
-    }
-
-    if (!fs.existsSync(path.resolve(reportPath, 'js'))){
-        fs.mkdirSync(path.resolve(reportPath, 'js'));
-        let master = fs.readFileSync(path.resolve(__dirname, '../../performance-reports/js/master.js')).toString();
-        fs.writeFileSync(path.resolve(reportPath, './js/master.js'), master);
-    }else{
-        let master = fs.readFileSync(path.resolve(__dirname, '../../performance-reports/js/master.js')).toString();
-        fs.writeFileSync(path.resolve(reportPath, './js/master.js'), master);
-    }
-
-    if (!fs.existsSync(path.resolve(reportPath, 'css'))){
-        fs.mkdirSync(path.resolve(reportPath, 'css'));
-        let css = fs.readFileSync(path.resolve(__dirname, '../../performance-reports/css/report.css')).toString();
-        fs.writeFileSync(path.resolve(reportPath, './css/report.css'), css);
-    }
-
+function updateReports(report, name){
     if (!fs.existsSync(path.resolve(reportPath, './js/reports.js'))){
         let obj = {
             [name]: report
@@ -43,6 +22,16 @@ function saveReport(report, name){
         content = "var reports = " + data;
         fs.writeFileSync(path.resolve(reportPath, './js/reports.js'), content);
     }
+}
+
+function saveReport(report, name){
+
+    if (!fs.existsSync(reportPath)){
+        let srcPath = path.resolve(__dirname, '../../performance-reports');
+        copy(srcPath, appPath);
+    }
+    updateReports(report, name);
+
 }
 
 module.exports = saveReport;
